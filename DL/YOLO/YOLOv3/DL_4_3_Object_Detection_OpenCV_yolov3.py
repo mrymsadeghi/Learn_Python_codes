386c7d32b3270035f39c0a51b98b3434f49dbd4c
Maryam <the.maryam.sadeghi@gmail.com>
2022-08-24 09:08:51 +0200

    Yolov7 generate plates

---
+++ DL/YOLO/YOLOv3/DL_4_3_Object_Detection_OpenCV_yolov3.py
@@ -0,0 +1,66 @@
+import os
+import cv2 as cv
+import numpy as np
+
+yolo_path = "/data/YOLO"
+
+classes = []
+with open("/data/YOLO/coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+
+yolo_net = cv.dnn.readNet(os.path.join(yolo_path,"yolov3.weights"), os.path.join(yolo_path,"yolov3.cfg"))
+layer_names = yolo_net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in yolo_net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+cap = cv.VideoCapture(0)
+
+while(cap.isOpened()):
+    ret, img = cap.read()
+    if ret == True:
+        #img = cv.resize(frame, None, fx=0.8, fy=0.8)
+        height, width, channels = img.shape
+        print(height, width)
+        blob = cv.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        yolo_net.setInput(blob)
+        outs = yolo_net.forward(output_layers)
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    # Rectangle coordinates
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        print(indexes)
+        font = cv.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                color = colors[class_ids[i]]
+                cv.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                cv.putText(img, label, (x, y + 30), font, 2, color, 2)
+        cv.imshow('Frame',img)
+        if cv.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        break
+    
+cap.release()
+cv.destroyAllWindows()
