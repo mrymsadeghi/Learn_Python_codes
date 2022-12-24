424144a44b44754fa1b80de7346e2b31ffd301ee
Maryam <the.maryam.sadeghi@gmail.com>
2022-12-24 17:41:17 +0100

    semantic segmentation added

--- Small_projects/01_translate_subtitles.py
+++ Small_projects/01_translate_subtitles.py
@@ -1,9 +1,9 @@
 import os
 from googletrans import Translator
 
-filepath = "C:/"
-filename1 = "sub_S01E01.srt"
-filename2 = "sub_S01E01.txt"
+filepath = "C:/PyProjects/github/Learn_Python_codes/Small_projects"
+filename1 = "Bad.Sisters.S01E10.720p.WEB_.H264-GGEZ-No-HI.srt"
+filename2 = "Bad.Sisters.S01E10.720p.WEB_.H264-GGEZ-No-HI.txt"
 
 nums = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -24,6 +24,7 @@ with open(os.path.join(filepath, filename2), 'w', encoding="utf-8") as f:
             f.write('/n')
         else:
             if line_en:
+                print(line_en)
                 line_fa = translator.translate(line_en, src='en', dest='fa')
                 f.write(line_fa.text)
 
